f972b4ec1630ca723f8440a60ceeafcad38fa238
Sangmoo <first_1st@naver.com>
2023-12-31 23:48:41 +0900

    Streamlit - Multi Page

--- Home.py
+++ Home.py
@@ -1,9 +1,23 @@
 import streamlit as st
 
-st.selectbox(
-    "Choose your model",
-    (
-        "GPT-3",
-        "GPT-4",
-    ),
+st.set_page_config(
+    page_title="FullstackGPT Home",
+    page_icon="🤖",
+)
+
+st.markdown(
+    """
+# Hello!
+            
+Welcome to Sangmoo FullstackGPT Portfolio!
+            
+Here are the apps I made:
+            
+- [ ] [DocumentGPT](/DocumentGPT)
+- [ ] [PrivateGPT](/PrivateGPT)
+- [ ] [QuizGPT](/QuizGPT)
+- [ ] [SiteGPT](/SiteGPT)
+- [ ] [MeetingGPT](/MeetingGPT)
+- [ ] [InvestorGPT](/InvestorGPT)
+"""
 )
